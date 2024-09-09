bd0959a2035c870049e1f656eddc72fe02e28b82
zsotroav <zsotroav@gmail.com>
2024-09-09 11:50:39 +0200

    Cleanup branch and update with upstream inmprovements

--- include/config.h
+++ include/config.h
@@ -5,7 +5,7 @@
 /**
  * @brief Set to the exact amount of panels in the display. Never set it lower!!!
  */
-#define CFG_PANEL_COUNT 7
+#define CFG_PANEL_COUNT 1
 /**
  * @brief Set the byte polarity of the driver - 0 is normal, 1 is inverted
  */
@@ -15,6 +15,20 @@
  * @brief Enabling relative mode only updates the pixels that are different compared to the previous state
  */
 #define CFG_RELATIVE_MODE 1
+
+/**
+ * @brief Configures the energization time for each coil, in microseconds. Default value is 1000 us = 1 ms, from specification.
+ * Drastic write speed increase can be achieved using this. The value should determined experimentally, and it depends
+ * on the screen position. Eg. using the screen horizontally requires less magnetic energization than using vertically.
+ * 
+ * Reference values: A good condition vertical screen works well with 400 us write time. For a horizontal one I was able
+ * to decrease the value to 300 us. 
+ *
+ * 								!!! DANGER !!!
+ * Increasing this value over 1000 us might cause overcurrent in the driver IC's or overheating the
+ * windings which might destroy them.
+ */
+#define CFG_WRITE_TIME_US 400
 
 //PIN DEFINITIONS
 
